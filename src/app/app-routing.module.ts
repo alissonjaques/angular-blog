c5e2515df5e105d9e952d21409ac7c83c03321b7
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-10-13 11:38:06 -0300

    chore: adição da biblioteca de rotas do angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
